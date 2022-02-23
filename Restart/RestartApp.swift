5cc2097804353826a24f442328b8278f3b5e6e4d
Ted Butler <tedsbutler@gmail.com>
2022-02-22 23:48:12 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ted Butler on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
